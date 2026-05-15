3be11032678555dadf2934ca2cafd46112dd6435
Suyog Pokhrel <pokhrelsuyog79@gmail.com>
2026-05-15 15:15:04 +0545

    comments on contact

--- src/main/java/com/kickoff/controller/ContactServlet.java
+++ src/main/java/com/kickoff/controller/ContactServlet.java
@@ -6,15 +6,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+// Mapping this servlet to the "/contact" URL
 @WebServlet(asyncSupported = true, urlPatterns = {"/contact"})
 public class ContactServlet extends HttpServlet {
-
+    // Serial version UID for serialization
     private static final long serialVersionUID = 1L;
-
+    // Handles GET requests from the client
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Forwarding the request to Contact.jsp page
         request.getRequestDispatcher("/Pages/Root/Contact.jsp").forward(request, response);
     }
 }
