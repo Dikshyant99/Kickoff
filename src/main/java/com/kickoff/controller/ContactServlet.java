30068b106a3a60d8de879ab57aae9e7ab0428169
RodhanCoder <rodhanshrestha094@gmail.com>
2026-05-15 14:47:30 +0545

    Contact Servlet Change

--- src/main/java/com/kickoff/controller/ContactServlet.java
+++ src/main/java/com/kickoff/controller/ContactServlet.java
@@ -13,8 +13,8 @@ public class ContactServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("/Pages/Root/Contact.jsp").forward(request, response);
-    }
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException{
+    request.getRequestDispatcher("/Pages/Root/Contact.jsp").forward(request, response);
+}
 }
